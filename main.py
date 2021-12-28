386d997b09e8607c6c61593f26537b167316ef6d
kamivibra <kami_vibra@yahoo.com>
2021-12-28 16:33:32 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+# This file is created for python basics to intermediate level practice only. Uncomment any section to try it out. 
+# Variables
+# first_name = "Muhammad Kamran"
+# last_name = "Khan"
+# full_name = first_name + " " + last_name
+# print("Hello " + full_name)
+# Integers
+# Total_paid = 100
+# Deducted_amount = 57
+# total_balance = 100 - 57
+# print("Your change is "+str(total_balance))
+
+# Floating
+# height = 250.5
+# print("Your height is: "+str(height)+"cm")
+# print(type(height))
+
+# Booleans
+# Human = True
+# print("human")
+# print(type(Human))
+
+
+# Multiple Assignment = allows us to use multiple variables at the same time in one line of code.
+# first we will use it separately and then we will try to use it as multiple assignment.
+
+# name = "khan"
+# age = 35
+# hobby = "coding"
+# print(name)
+# print(age)
+# print(hobby)
+# Now lets try using it as multiple assignment
+
+# name, age, hobby = "khan", 36, "coding"
+
+# print(name)
+# print(age)
+# print(hobby)
+
+# Another example, First multiple lines of code.
+
+# Tom = 30
+# Jerry = 30
+# bean = 30
+# scooby = 30
+# print(Tom)
+# print(Jerry)
+# print(bean)
+# print(scooby)
+# Now we will perform same task using multiple assignment.
+
+# Tom = Jerry = bean = scooby = 30  #one line of code
+# print(Tom)
+# print(Jerry)
+# print(bean)
+# print(scooby)
+
+
+
+
+
+
